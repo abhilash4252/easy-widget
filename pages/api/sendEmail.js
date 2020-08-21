537d8f7e94260b5970e57d0e81b30b2b128a1867
Abhilash Reddy <abhilash4252@gmail.com>
2020-08-21 13:48:48 +0530

    Enabled Cors

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,8 +1,21 @@
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
 import { sendEmail } from "../../utils/sendEmail";
 import { getUserInfo } from "../../utils/auth/firebaseAdmin";
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
 export default async (req, res) => {
   try {
+    // Run cors
+    await cors(req, res);
     const { id, feedback } = req.body;
     await getUserInfo(id);
     await sendEmail({ email: id, feedback });
