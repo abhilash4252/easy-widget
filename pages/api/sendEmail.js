5d914902a5e04b33449ceeecd0169d19041c4047
Abhilash Reddy <abhilash4252@gmail.com>
2020-08-21 12:13:36 +0530

    Arguments strong type

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
   try {
     const { id, feedback } = req.body;
     await getUserInfo(id);
-    await sendEmail({ id, feedback });
+    await sendEmail({ email: id, feedback });
     return res.status(200).end();
   } catch (error) {
     return res.status(422).send(error);
